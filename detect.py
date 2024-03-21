dd26278e096f242c21eb8fe47ee76ef916782b97
boris <b.smeenk@hr.nl>
2024-03-21 08:34:07 +0100

    minor fixes & changes

--- detect.py
+++ detect.py
@@ -1,38 +1,50 @@
 import os
+import random
 
-from ultralytics import YOLO
 import cv2
+from ultralytics import YOLO
 
-TRESHOLD = 0.5
 
+TRESHOLD = 0.8
+
+model = YOLO("cards.pt") # model file
 cap = cv2.VideoCapture(0)
-model_path = os.path.join('.', 'runs', 'detect', 'train3', 'weights', 'last.pt')
-model = YOLO(model_path)
 
-while(True):
-    ret, frame = cap.read()
-    H, W, _ = frame.shape
+colors = [
+    (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    for name in model.names
+] # generate random colors for each class
 
-    results = model(frame)[0]
+while True:
+    ret, frame = cap.read() # read webcam input
+    frame = cv2.flip(frame, 1) # flip horizontal
+    results = model(frame)[0] # inference
 
     for result in results.boxes.data.tolist():
-        x1, x2, y1, y2, score, class_id = result
-
-        print(score)
+        x1, y1, x2, y2, score, class_id = result
 
         if score > TRESHOLD:
-            v2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-            cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+            # only draw bounding boxes when we meet our treshold
+            cv2.rectangle(
+                frame, (int(x1), int(y1)), (int(x2), int(y2)), colors[int(class_id)], 3
+            ) # draw the bounding box
+            cv2.putText(
+                frame,
+                results.names[int(class_id)].upper(),
+                (int(x1), int(y1 - 10)),
+                cv2.FONT_HERSHEY_DUPLEX,
+                1.3,
+                colors[int(class_id)],
+                3,
+                cv2.LINE_AA,
+            ) # draw the class name
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+    # display the resulting frame
+    cv2.imshow("Object detection with YOLOv8", frame)
 
-    # Break the loop on pressing 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # break the loop on pressing 'q'
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# After the loop release the cap object
 cap.release()
-# Destroy all the windows
 cv2.destroyAllWindows()
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # build a new model from scratch
 model.train(data="config.yaml", epochs=100)  # train the model
